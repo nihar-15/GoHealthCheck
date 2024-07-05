faf7c4bdc98e19740a5e1c7da68346eb13cde252
nihar-15 <kumarniharika684@gmail.com>
2024-07-05 13:30:59 -0400

    Completed the basic code

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 func main() {
 	app := &cli.App{
 		Name:  "Healthchecker",
-		Usage: "A tiny tool that checks the given domain is down.",
+		Usage: "A tiny tool that checks if the given domain is down.",
 		Flags: []cli.Flag{
 			&cli.StringFlag{
 				Name:     "domain",
@@ -28,7 +28,7 @@ func main() {
 		},
 		Action: func(c *cli.Context) error {
 			port := c.String("port")
-			if c.String("port") == "" {
+			if port == "" {
 				port = "80"
 			}
 			status := Check(c.String("domain"), port)
